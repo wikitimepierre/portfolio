3bcb706239bbbed6268ce143a9ca472195f95e7d
Pierre Dumas <wikitime.pierre@gmail.com>
2024-04-12 00:04:25 +0200

    feat: added "brève description" des projets.

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,12 +7,14 @@ import IconDisplay2 from './IconDisplay2.js';
 function Home() {
   return (
     <div className='general-container'>
-      <div className='pierre-hometitle'>
-        Portfolio de Pierre Dumas
-        <div className='pierre-homesubtitle'>https://github.com/wikitimepierre/persoOC</div>
+      <div>
+        <h1>
+          Portfolio de Pierre Dumas
+        </h1>
+        <Link to="https://github.com/wikitimepierre/persoOC" className='pierre-homesubtitle'>https://github.com/wikitimepierre/persoOC</Link>
       </div>
-      <div className='home-container'>
 
+      <div className='home-container'>
         <Link to="/1-CV/">
           <div className="pierre-card"><button class="pierre-wrap pierre-button-home pierre-button-cv">
             PIERROT<br />my Resume online<div className='pierre-fullSkill'><IconDisplay2 iconFileName="html5" /><IconDisplay2 iconFileName="css3" /><IconDisplay2 iconFileName="sass" /></div>
@@ -32,8 +34,26 @@ function Home() {
           <div className="pierre-card"><button class="pierre-wrap pierre-button-home pierre-button-wikitime">
             WIKITIME<br />surprise<div className='pierre-fullSkill'><IconDisplay2 iconFileName="question" /><IconDisplay2 iconFileName="question" /><IconDisplay2 iconFileName="question" /><IconDisplay2 iconFileName="question" /></div>
           </button></div></Link>
-
       </div >
+
+      <div>
+        Le projet CV est un projet de site de CV en ligne.<br />
+        Il est inspiré d'un cv html (https://www.w3schools.com/howto/howto_website_create_resume.asp)<br />
+        aue j'ai cherché à rendre visuellement attractif.<br />
+        et que j'ai porté en REACT.<br />
+        <br />
+        Le projet Shop est un projet de site de vente en ligne et comprend une liste de produits et un panier de commande.<br />
+        J'ai cherché à l'améliorer visuellement et à je l'ai rendu responsif.<br />
+        On peut sélectionner des produits qui s'ajoutent au panier et augmentent le montant total.<br />
+        Il est inspiré du P6, c'est l'un des exemples utilisé par le professeur pour expliquer React.<br />
+        <br />
+        Le projet Bank est un projet de gestion de compte bancaire.<br />
+        Il est inspiré du P11, son objectif est de gérer le signin et le state RTK.<br />
+        Ici, j'ai gardé la gestion du state qui affiché dans le menu debug.<br />
+        <br />
+        Le projet Wikitime est un projet personnel que je présenterai.<br />
+        Pour des raisons de confidentialité et de WIP, il n'est pas disponible sur github.<br />
+      </div>
     </div >
   );
 }
